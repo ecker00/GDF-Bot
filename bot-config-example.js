f900618138b4670539235149e8b1f7d805e1d3d3
Christer Egon Eckermann <ecker00@gmail.com>
2020-02-18 06:38:03 +0100

    Prettified

--- bot-config-example.js
+++ bot-config-example.js
@@ -1,24 +1,23 @@
 module.exports = {
-
   // Bot login token
-  token: '123',
+  token: "123",
 
   // Auto roles config
   autoRoles: {
-    listenChannel: 'myRoleRequestl',
-    availableRoles: ['Role1', 'Role2', 'Role3'] // Roles users can give themselves
+    listenChannel: "myRoleRequestl",
+    availableRoles: ["Role1", "Role2", "Role3"] // Roles users can give themselves
   },
 
   // Daily challenge config
   dailyChallenge: {
-    listenChannel: 'myChallengeLog',
-    responseChannel: 'myChallengeChat',
+    listenChannel: "myChallengeLog",
+    responseChannel: "myChallengeChat",
     milestones: [
       {
-        day: 7, 
-        image: '/images/Day 7 badge.png',
-        personalMessage: 'Congratulations on reaching day 7, good going!',
-        publicMessage: '%s just reached day 7! Let\'s cheer them on! 💪'
+        day: 7,
+        image: "/images/Day 7 badge.png",
+        personalMessage: "Congratulations on reaching day 7, good going!",
+        publicMessage: "%s just reached day 7! Let's cheer them on! 💪"
       }
     ]
   }
